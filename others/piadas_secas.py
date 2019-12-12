2402ba18b931490967cb91a6585c2f775fc5c045
Souocare <38910349+souocare@users.noreply.github.com>
2019-12-12 22:19:02 +0000

    added piadas_secas py file

---
+++ others/piadas_secas.py
@@ -0,0 +1,15 @@
+import json
+from random import randrange
+import requests
+
+
+def get_piada_seca():
+    org_file = requests.get(url="https://raw.githubusercontent.com/souocare/bot-portuguese-publictransports/master/others/piadas_secas.json",
+                                        headers={"Accept": "application/json; charset=utf-8"})
+
+    org_file.encoding = "ISO 3166-2"
+    file = org_file.json()
+
+    piada = file[str(randrange(100))]
+
+    return piada
